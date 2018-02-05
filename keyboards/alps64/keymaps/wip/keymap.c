e9895086276e1547e52037c07f531c2a9cef3ccb
Gregory Eremin <g@erem.in>
2018-02-05 12:15:33 +0100

    Swap forward/play keys

--- keyboards/alps64/keymaps/wip/keymap.c
+++ keyboards/alps64/keymaps/wip/keymap.c
@@ -20,7 +20,7 @@ KC_LCTL,  KC_LALT, KC_LGUI,                 KC_SPC,                             
 KC_GRV,      KC_F1,       KC_F2,       KC_F3,      KC_F4,      KC_F5,      KC_F6,    KC_F7,    KC_F8,    KC_F9,     KC_F10,      KC_F11,      KC_F12,      _______,     KC_DEL, \
 KC_LALT,     PH(KC_Q),    PH(KC_W),    PH(KC_E),   PH(KC_R),   PH(KC_T),   PH(KC_Y), PH(KC_U), KC_UP,    PH(KC_O),  PH(KC_P),    KC_VOLD,     KC_VOLU,     KC_MUTE,     \
 _______,     KC_F13,      KC_F16,      KC_F17,     KC_F18,     PH(KC_G),   PH(KC_H), KC_LEFT,  KC_DOWN,  KC_RIGHT,  PH(KC_SCLN), PH(KC_QUOT),              PH(KC_ENT),  \
-PH(KC_LSFT), _______,     PH(KC_Z),    PH(KC_X),   PH(KC_C),   PH(KC_V),   PH(KC_B), PH(KC_N), PH(KC_M), KC_MRWD,   KC_MPLY,     KC_MFFD,                  PH(KC_RSFT), _______, \
+PH(KC_LSFT), _______,     PH(KC_Z),    PH(KC_X),   PH(KC_C),   PH(KC_V),   PH(KC_B), PH(KC_N), PH(KC_M), KC_MRWD,   KC_MFFD,     KC_MPLY,                  PH(KC_RSFT), _______, \
 PH(KC_LCTL), PH(KC_LALT), PH(KC_LGUI),                         PH(KC_SPC),                                          _______,     PH(KC_RGUI), PH(KC_RALT), PH(KC_RCTL)  ),
 
 // Done with layers
