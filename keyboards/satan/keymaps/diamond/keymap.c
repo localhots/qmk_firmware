8f71d817e9decdbf2a7fc14237d1699b98b11f0a
Gregory Eremin <g@erem.in>
2018-02-05 12:15:51 +0100

    Restore poker layout, add backlighting controls

--- keyboards/satan/keymaps/diamond/keymap.c
+++ keyboards/satan/keymaps/diamond/keymap.c
@@ -32,15 +32,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,  KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC, KC_RBRC,KC_BSLS, \
   MO(_FL), KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,         KC_ENT,  \
   KC_LSFT,         KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,         KC_RSFT, \
-  _______, KC_LCTL,KC_LGUI,                KC_SPC,                                 KC_RALT,MO(_FL), MO(_FL),_______),
+  KC_LCTL, KC_LALT,KC_LGUI,                KC_SPC,                                 KC_RGUI,MO(_FL), KC_RALT,KC_RCTL),
 
 
 [_FL] = KEYMAP_ANSI(
   KC_GRV,      KC_F1,        KC_F2,          KC_F3,      KC_F4,      KC_F5,    KC_F6,    KC_F7,    KC_F8,       KC_F9,      KC_F10,      KC_F11,      KC_F12,     KC_DEL,  \
   KC_LALT,     LGUI(KC_GRV), LGUI(KC_TILDE), PH(KC_E),   PH(KC_R),   PH(KC_T), PH(KC_Y), PH(KC_U), KC_UP,       PH(KC_O),   PH(KC_P),    KC_VOLD,     KC_VOLU,    KC_MUTE, \
   _______,     KC_F13,       KC_F16,         KC_F17,     KC_F18,     PH(KC_G), PH(KC_H), KC_LEFT,  KC_DOWN,     KC_RIGHT,   PH(KC_SCLN), PH(KC_QUOT), PH(KC_ENT), \
-  PH(KC_LSFT), LCTL(KC_Z),   LCTL(KC_X),     LCTL(KC_C), LCTL(KC_V), PH(KC_B), PH(KC_N), PH(KC_M), KC_MRWD,     KC_MPLY,    KC_MFFD,     PH(KC_RSFT), \
-  _______,     PH(KC_LCTL),  PH(KC_LGUI),                PH(KC_SPC),                               PH(KC_RALT), _______,    _______,     _______ ),
+  PH(KC_LSFT), BL_DEC,       BL_INC,         BL_TOGG,    PH(KC_V),   PH(KC_B), PH(KC_N), PH(KC_M), KC_MRWD,     KC_MFFD,    KC_MPLY,     PH(KC_RSFT), \
+  PH(KC_LCTL), PH(KC_LALT),  PH(KC_LGUI),                PH(KC_SPC),                               PH(KC_RGUI), _______,    PH(KC_RALT), PH(KC_RCTL)),
 };
 
 enum function_id {
